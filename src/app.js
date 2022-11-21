8d3fe42ed33fb5756fa88a85b5156f1e6be892bd
Zekumoru Dragonhart <dev@zekumoru.com>
2022-11-21 18:24:33 +0100

    Commit source files for save

--- src/app.js
+++ src/app.js
@@ -1,2 +1,12 @@
 import 'normalize.css';
 import './style.css';
+import SvgLoader from '../scripts/SvgLoader';
+
+(async () => {
+  const svg = await SvgLoader.load('images/picture.svg', {
+    colorable: true,
+    style: 'color: red',
+    wrap: true,
+  });
+  document.body.appendChild(svg);
+})();
