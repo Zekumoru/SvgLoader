6256546148017f57d7ebac04d52a67883a6822f8
Zekumoru Dragonhart <dev@zekumoru.com>
2022-11-22 10:16:47 +0100

    Move SvgLoader to root

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import 'normalize.css';
 import './style.css';
-import SvgLoader from '../scripts/SvgLoader';
+import SvgLoader from '../SvgLoader';
 
 (async () => {
   const svg = await SvgLoader.load('images/picture.svg', {
